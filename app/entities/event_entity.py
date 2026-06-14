81ff18e48261506b4476f442de6747e278bf2548
Luis fernando Soto bedoya <luisfersotobedoya1123@gmail.com>
2026-06-14 13:55:41 -0500

    event entity and event model added

---
+++ app/entities/event_entity.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String, Integer, Column, DateTime
+from database.connection import Base
+
+class EventTable(Base):
+    __tablename__ = "events"
+
+    id = Column(Integer, primary_key= True, index= True)
+    username = Column(String, nullable = False)
+    ip_address = Column(String, nullable = False)
+    event_type = Column(String, nullable = False)
+    resource = Column(String, nullable = False)
+    timestamp= Column(DateTime, nullable= False)
