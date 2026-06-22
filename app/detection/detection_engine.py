23a0d8f69cc39b52fc1b99592d24d44acd5e2edb
Luis fernando Soto bedoya <luisfersotobedoya1123@gmail.com>
2026-06-21 19:08:39 -0500

    refactoring(run_detection_engine): rebuild of the run detecion engine method

--- app/detection/detection_engine.py
+++ app/detection/detection_engine.py
@@ -1,29 +1,27 @@
 from detection.brute_force import detect_brute_force
 from detection.resource_enumeration import detect_resource_enumeration
-from sqlalchemy import Session
 
 
-def run_detection_engine(db: Session, event):
+
+def run_detection_engine(db, event):
     alerts = []
     brute_force = detect_brute_force(
         db=db,
         username = event.username,
         source_ip = event.source_ip
     )
-    if brute_force:
-        alerts.append(brute_force)
-        return alerts
-    else:
-        None
-        
     resource_enumeration = detect_resource_enumeration(
         db = db,
         username = event.username,
         source_ip = event.source_ip
     )
-    if resource_enumeration:
+    if brute_force:
+        alerts.append(brute_force)
+        return alerts
+
+    elif resource_enumeration:
         alerts.append(resource_enumeration)
         return alerts
     else:
-        None
+        return None
 
