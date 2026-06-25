6c8c68678cc060bda0e5f525696dd21bf1da4c42
Luis fernando Soto bedoya <luisfersotobedoya1123@gmail.com>
2026-06-25 13:13:33 -0500

    build: modules resource_enumeration_attack and credential_stuffing_attack built

--- app/generator/attack_runner.py
+++ app/generator/attack_runner.py
@@ -27,3 +27,54 @@ def run_brute_force_attack(db : Session, username : str, ip_address : str, attem
             )
             generated_alerts.append(saved_alert)
     return generated_events, generated_alerts  
+
+def run_resource_enumeration_attack(db: Session, username : str, ip_address : str, resources : list[str]):
+    generated_events = []
+    generated_alerts = []
+    for resource in resources:
+        event = generate_event(
+            db = db,
+            username = username,
+            ip_address = ip_address,
+            event_type = "RESOURCE_ACCESS",
+            resource = resource
+        )
+        generated_events.append(event)
+
+        alerts = run_detection_engine(
+            db = db,
+            event = event
+        )
+        for alert in alerts:
+            saved_alert = save_alert(
+                db = db,
+                alert_model = alert
+            )
+            generated_alerts.append(saved_alert)
+    return generated_events, generated_alerts
+
+def run_credential_stuffing_attack(db : Session, username : list[str], ip_address : str):
+    generated_events = []
+    generated_alerts = []
+
+    for usernames in username:
+        event = generate_event(
+            db = db,
+            username = usernames,
+            ip_address = ip_address,
+            event_type = "LOGIN_FAILED",
+            resource = "/login"
+        )
+        generated_events.append(event)
+        alerts = run_detection_engine(
+            db = db,
+            event = event
+        )
+        for alert in alerts:
+            saved_alerts = save_alert(
+                db = db,
+                alert_model = alert
+            )
+            generated_alerts.append(saved_alerts)
+    return generated_events, generated_alerts
+
--- app/repositories/event_repository.py
+++ app/repositories/event_repository.py
@@ -3,8 +3,8 @@ from entities.event_entity import EventTable
 from models.event_model import EventModel
 from datetime import datetime, timedelta
 from config.detection_rules import BRUTE_FORCE_INTERVAL_MINUTES
-from config.detection_rules import RESOURCE_ENUMERATION_MINUTES
-from config.detection_rules import CREDENTIAL_STUFFING_MINUTES
+from config.detection_rules import RESOURCE_ENUMERATION_INTERVAL_MINUTES
+from config.detection_rules import CREDENTIAL_STUFFING_INTERVAL_MINUTES
 
 def save_event(db : Session, event_model: EventModel):
     try:
@@ -29,7 +29,7 @@ def count_failed_logins_by_username(db: Session, username: str):
     )
 
 def get_unique_resources_by_username(db : Session, username: str):
-       start_time = datetime.utcnow() - timedelta(minutes = RESOURCE_ENUMERATION_MINUTES)
+       start_time = datetime.utcnow() - timedelta(minutes = RESOURCE_ENUMERATION_INTERVAL_MINUTES)
        resources =  db.query(EventTable.resource).filter(EventTable.username == username).filter(EventTable.event_type == "RESOURCE_ACCESS").filter(EventTable.event_timestamp >= start_time).distinct().all()
        return[
             row[0]
@@ -37,14 +37,14 @@ def get_unique_resources_by_username(db : Session, username: str):
        ]
     
 def get_unique_usernames_by_ip(db: Session, ip_address : str):
-     start_time = datetime.utcnow() - timedelta(minutes = CREDENTIAL_STUFFING_MINUTES)
+     start_time = datetime.utcnow() - timedelta(minutes = CREDENTIAL_STUFFING_INTERVAL_MINUTES)
      usernames = db.query(EventTable.username).filter(EventTable.ip_address == ip_address).filter(EventTable.event_type == "LOGIN_FAILED").filter(EventTable.event_timestamp >= start_time ).distinct().all()
      return[
           row[0]
           for row in usernames
      ]
 
-def count_failed_login_by_username_since(db : Session, username : str):
+def count_failed_logins_by_username_since(db : Session, username : str):
      start_time = datetime.utcnow() - timedelta(minutes = BRUTE_FORCE_INTERVAL_MINUTES)
      return(
      db.query(EventTable).filter(EventTable.username == username).filter(EventTable.event_type == "LOGIN_FAILED").filter(EventTable.event_timestamp >= start_time).count()
